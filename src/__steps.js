79865026ed925ad587ad844ab6de802082c2dc3c
Ashiqur Rahman <imashiqe@gmail.com>
2022-10-24 11:22:08 +0600

    bootstrap added

---
+++ src/__steps.js
@@ -0,0 +1,9 @@
+/*
+1. create  firebase app
+2. get firebase configuration inside firebase.config.js
+3. make  sure to export app from  firebase config
+4. create  a react app 
+5. install  firebase  and  react  router  dom
+6. install  bootstrap  and react-bootstrap
+7. make sure to  import bootstrap  css in  the  index.js  file
+*/
