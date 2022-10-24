85938435a6cfc47d739d0bd2218aa4f6207a04e3
Ashiqur Rahman <imashiqe@gmail.com>
2022-10-24 11:37:27 +0600

    different router file and  router setup

--- src/__steps.js
+++ src/__steps.js
@@ -1,4 +1,5 @@
 /*
+BASIC  PROJECT SETUP
 1. create  firebase app
 2. get firebase configuration inside firebase.config.js
 3. make  sure to export app from  firebase config
@@ -6,4 +7,10 @@
 5. install  firebase  and  react  router  dom
 6. install  bootstrap  and react-bootstrap
 7. make sure to  import bootstrap  css in  the  index.js  file
+8. 
+*/
+
+/*
+ROUTER  SETUP
+1. 
 */
