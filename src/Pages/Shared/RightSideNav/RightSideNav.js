42fed372e8acb8d9897d6ee8a7d021c3549e0c72
imashiqe <imashiqe@gmail.com>
2022-10-29 01:40:10 +0600

    react icon aded

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2>this is right</h2>
+           <ButtonGroup vertical>
+      
+
+             <Button variant="outline-primary">Login With  Google</Button>
+              <Button variant="outline-dark">Login With Github</Button>
+
+     
+          </ButtonGroup>
         </div>
     );
 };
