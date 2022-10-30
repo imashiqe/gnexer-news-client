3255848b9037992ad8fddb2039522e945a8b090f
imashiqe <imashiqe@gmail.com>
2022-10-30 15:56:33 +0600

    news  data load  and route added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import NewsSummaryCard from '../Shared/NewsSummaryCard/NewsSummaryCard';
 
 const Home = () => {
+    const allnews = useLoaderData();
+
     return (
         <div>
-            <h2>This is Home Components</h2>
+            <h2>Gnexer News : {allnews.length}</h2>
+            {
+              allnews.map(news => <NewsSummaryCard 
+                key={news._id}
+                news={news}
+              ></NewsSummaryCard>)
+            }
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -3,13 +3,14 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 import LeftSideNav from './LeftSideNav/LeftSideNav';
 const Header = () => {
     return (
         <div>
             <Navbar collapseOnSelect className='mb-4' expand="lg" bg="light" variant="light">
       <Container>
-        <Navbar.Brand href="#home">Gnexer News</Navbar.Brand>
+        <Navbar.Brand ><Link to='/'>Gnexer News</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
