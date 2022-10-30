f511025cd7b4474fd19e8d7fc20f226f563c0321
imashiqe <imashiqe@gmail.com>
2022-10-30 11:04:45 +0600

    navbar added with responsive

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -44,8 +44,7 @@ const RightSideNav = () => {
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -56,8 +55,7 @@ const RightSideNav = () => {
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+         
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -68,10 +66,7 @@ const RightSideNav = () => {
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+         
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
