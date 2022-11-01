12058a62959d183ac0ddccddfb642cc648185d29
imashiqe <imashiqe@gmail.com>
2022-11-01 17:42:04 +0600

    env localadded

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDlv1kC253B6FJYrVPVDysKXdmJpNajjec",
-  authDomain: "gnexer-news.firebaseapp.com",
-  projectId: "gnexer-news",
-  storageBucket: "gnexer-news.appspot.com",
-  messagingSenderId: "215806009497",
-  appId: "1:215806009497:web:21306483cbacacf1bb5808"
+  apiKey: process.env.REACT_APP_apiKey,  
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appID,
 };
 
 // Initialize Firebase
